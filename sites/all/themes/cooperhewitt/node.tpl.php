2e197f7173976c32c1ee3752400c91d73639cfd6
Micah Walter <micah@micahwalter.com>
2011-05-05 21:31:03 -0400

    moved buttons

--- sites/all/themes/cooperhewitt/node.tpl.php
+++ sites/all/themes/cooperhewitt/node.tpl.php
@@ -36,6 +36,7 @@
                                             else
                                                 echo $name;
                                             ?></span>
+<span  class='st_twitter_large' ></span><span  class='st_facebook_large' ></span><span  class='st_yahoo_large' ></span><span  class='st_gbuzz_large' ></span><span  class='st_email_large' ></span><span  class='st_sharethis_large' ></span>
 					</div>
 					<div class="iteminfo">
 	
@@ -54,7 +55,6 @@
 			<?php endif; ?>	
 
 							
-			<span  class='st_twitter_large' ></span><br /><span  class='st_facebook_large' ></span><br /><span  class='st_yahoo_large' ></span><br /><span  class='st_gbuzz_large' ></span><br /><span  class='st_email_large' ></span><br /><span  class='st_sharethis_large' ></span><br />
 			
 			<?php print $content ?>
 			    
