c34a5064f39f2872f4eb1e5c0973f9165e0305ba
Micah Walter <micah@lithium.si.edu>
2011-06-14 13:51:31 -0400

    upgrading to 6.22

--- modules/AjaxLogin/ajaxlogin.js
+++
@@ -1,9 +0,0 @@
-// $Id: ajaxlogin.js 316 2009-02-07 21:47:11Z lucky $
-
-// Add the thickbox class to the login links - borrowed from the Thickbox module 
-Drupal.behaviors.initAjaxLogin = function(context) {
- 
-	// Re-write our sign in links
-    $("a[@href*='/user/login']").addClass('thickbox').each(function() { this.href = this.href.replace(/user\/login(%3F|\?)?/,"user/login/ajaxlogin?height=230&width=415&") });
-    $("a[@href*='?q=user/login']").addClass('thickbox').each(function() { this.href = this.href.replace(/user\/login/,"user/login/ajaxlogin&height=230&width=415") });
-}
