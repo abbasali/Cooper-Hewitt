f3829dde70cf1aebdc1f0457a365d79968bf7ece
Micah Walter <micah@micahwalter.com>
2011-06-23 10:29:08 -0400

    removed title and extra space from redesign

--- sites/all/themes/redesign/templates/page.tpl.php
+++ sites/all/themes/redesign/templates/page.tpl.php
@@ -151,9 +151,6 @@
 	  	<?php print $breadcrumb; ?>
 	  	<div class="section">
         
-        <?php if ($title): ?>
-          <h1 class="title"><?php print $title; ?></h1>
-        <?php endif; ?>
         <?php print $messages; ?>
         <?php if ($tabs): ?>
           <div class="tabs"><?php print $tabs; ?></div>
