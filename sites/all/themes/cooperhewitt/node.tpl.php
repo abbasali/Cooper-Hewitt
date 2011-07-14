bfb902e518ae456037c31c4b6a94962179310ab2
Micah Walter <micah@micahwalter.com>
2011-07-14 16:07:34 -0400

    added comment link and rearanged header

--- sites/all/themes/cooperhewitt/node.tpl.php
+++ sites/all/themes/cooperhewitt/node.tpl.php
@@ -4,6 +4,7 @@
 /**
  * @file node.tpl.php
  **/
+$link = '';
 ?>
 <div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clear-block">
 	
@@ -27,26 +28,34 @@
 			<?php if ($submitted): ?>	
 				  		
 				<div class="article-info-surround">
+					
+					
 					<div class="article-info-right">
-					<span class="createdby"><?php
-                                            $uprofile = user_load(array('uid'=>$node->uid));
-                                            $user_fname = $uprofile->profile_;
-                                            if ($user_fname!="")
-                                                echo l($user_fname,'user/'.$node->uid);
-                                            else
-                                                echo $name;
-                                            ?>
+					<span  class='st_twitter_vcount' st_url="<?php echo 'http://cooperhewitt.org/node/'.$node->nid ?>" st_title="<?php echo $title ?>" displayText='Tweet'></span><span  class='st_facebook_vcount' st_url="<?php echo 'http://cooperhewitt.org/node/'.$node->nid ?>" st_title="<?php echo $title ?>" displayText='Facebook'></span><span  class='st_plusone_vcount' st_url="<?php echo 'http://cooperhewitt.org/node/'.$node->nid ?>" st_title="<?php echo $title ?>" ></span><span  class='st_fblike_vcount' st_url="<?php echo 'http://cooperhewitt.org/node/'.$node->nid ?>" st_title="<?php echo $title ?>" ></span>
 					</div>
 					<div class="iteminfo">
-	
-						<div class="article-info-left">
+					<div class="article-info-left">
 							<h2><?php print l($node->title,'node/'.$node->nid); ?></h2>
 							<span class="createdate">
 							
-								<span class="date1"><?php print format_date($node->created, 'custom', "F j, Y") ?></span>
+								<span class="date1"><?php print format_date($node->created, 'custom', "F j, Y") ?> | </span>
 							</span>
+							<span class="createdby"><?php
+		                                            $uprofile = user_load(array('uid'=>$node->uid));
+		                                            $user_fname = $uprofile->profile_;
+		                                            if ($user_fname!="")
+		                                                echo l($user_fname,'user/'.$node->uid);
+		                                            else
+		                                                echo $name;
+		                                            ?>
+		
+					                                
+	| <?php $comments = 'node/'.$node->nid; 
+	print l('Leave a comment', $comments);
+	?>
+	</span>
 							<div class="clr"></div>
-						</div>
+					</div>
 	
 					</div>
 				</div>
@@ -56,12 +65,11 @@
 							
 			
 			<?php print $content ?>
-			    
-		
+			
 		</div>
 		
 		<div class="module-bm">
-			<div class="module-bl"><span  class='st_twitter_vcount' displayText='Tweet'></span><span  class='st_email_vcount' displayText='Email'></span><span  class='st_facebook_vcount' displayText='Facebook'></span><span  class='st_plusone_vcount' ></span><span  class='st_fblike_vcount' ></span></div>
+			<div class="module-bl"></div>
 			<div class="module-br"></div>
 		</div>
 
