470f84c00c403deff34a739f891c2ea380fb3122
Micah Walter <micah@micahwalter.com>
2011-07-14 12:10:46 -0400

    rmeoved hard coded share this

--- sites/all/themes/cooperhewitt/node.tpl.php
+++ sites/all/themes/cooperhewitt/node.tpl.php
@@ -35,8 +35,7 @@
                                                 echo l($user_fname,'user/'.$node->uid);
                                             else
                                                 echo $name;
-                                            ?></span>
-<span  class='st_twitter_large' ></span><span  class='st_facebook_large' ></span><span  class='st_yahoo_large' ></span><span  class='st_gbuzz_large' ></span><span  class='st_email_large' ></span><span  class='st_sharethis_large' ></span>
+                                            ?>
 					</div>
 					<div class="iteminfo">
 	
--- sites/all/themes/cooperhewitt/page.tpl.php
+++ sites/all/themes/cooperhewitt/page.tpl.php
@@ -36,7 +36,6 @@
 		<link href="<?php echo base_path() . path_to_theme(); ?>/css/general.css" rel="stylesheet" type="text/css" />
 <!--[if IE 7]><link type="text/css" rel="stylesheet" media="all" href="<?php print base_path().path_to_theme() ?>/css/ie7.css" /><![endif]-->
 <!--[if IE 8]><link type="text/css" rel="stylesheet" media="all" href="<?php print base_path().path_to_theme() ?>/css/ie8.css" /><![endif]-->
-<script type="text/javascript">var switchTo5x=true;</script><script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script><script type="text/javascript">stLight.options({publisher:'808fd4d2-4b3f-457d-afda-337d5e5aa268'});</script>
 
 <script>
 	if (document.cookie.indexOf('visited=true') === -1) {
