8482d9feb3be75da9163f9b9d6cea77538d6105f
Micah Walter <micah@micahwalter.com>
2011-07-14 21:55:03 -0400

    fixed up links

--- sites/all/themes/cooperhewitt/node.tpl.php
+++ sites/all/themes/cooperhewitt/node.tpl.php
@@ -4,10 +4,8 @@
 /**
  * @file node.tpl.php
  **/
-$link = '';
 ?>
-<div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clear-block">
-	
+<div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clear-block">	
 	<div class="full-article">
 		<div class="module-tm">
 			<div class="module-tl"></div>
@@ -20,7 +18,7 @@ $link = '';
                             <?php if ($node->type!="blog") { ?>
                             <?php } else {
                                 $link = $node->links["comment_add"]["href"];
-                                ?>
+                            ?>
                                 <h2 class="contentheading"><?php echo l($title,$link);?></h2>
                             <?php } ?>
 			</div>
@@ -28,8 +26,7 @@ $link = '';
 			<?php if ($submitted): ?>	
 				<div class="article-info-surround">
 					<div class="iteminfo">
-							<h1 style="font-size:36px;line-height:1.4em;font-family: minerva-modern-1,minerva-modern-2;" ><?php print l($node->title,'node/'.$node->nid); ?></h1>
-							
+							<h1 style="font-size:36px;line-height:1.4em;font-family: minerva-modern-1,minerva-modern-2;" ><?php print l($node->title,$node->path); ?></h1>
 							 Written by 
 									<?php
 		                            $uprofile = user_load(array('uid'=>$node->uid));
@@ -40,19 +37,18 @@ $link = '';
 		                                    echo $name;
 		                            ?>
 							on <?php print format_date($node->created, 'custom', "F j, Y") ?> |  
-							<?php 
-								$comments = 'node/'.$node->nid; 
-								print l('Leave a comment', $comments);
-							?>
+							<a href="<?php echo $node->path ?>#disqus_thread">Comments</a><br /><br />
+							<span  class='st_twitter' st_title="<?php echo $node->title ?>" st_url="http://cooperhewitt.org/<?php echo $node->path ?>" displayText='Tweet'></span><span  class='st_facebook' st_title="<?php echo $node->title ?>" st_url="http://cooperhewitt.org/<?php echo $node->path ?>" displayText='Facebook'></span><span  class='st_email' st_title="<?php echo $node->title ?>" st_url="http://cooperhewitt.org/<?php echo $node->path ?>" displayText='Email'></span><span  class='st_sharethis' st_title="<?php echo $node->title ?>" st_url="http://cooperhewitt.org/<?php echo $node->path ?>" displayText='ShareThis'></span>
 							<div class="clr"></div>
 					</div>
 				</div>
 			<?php endif; ?>	
 
 			<?php print $content ?>
+		
 			</div>
 		
-		<div class="module-bm">
+		<div class="module-bm">	
 			<div class="module-bl"></div>
 			<div class="module-br"></div>
 		</div>
